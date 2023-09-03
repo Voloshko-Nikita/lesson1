2600a585e78bbe06c3226b29869902981d94ebb0
Voloshko-Nikita <nikita.voloshko.music@gmail.com>
2023-09-03 17:44:01 +0300

    Homework, Arrays

--- Boolean lgebra and flags/src/Main.java
+++ Boolean lgebra and flags/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         System.out.println("----Homework 1");
 
-        boolean weekday = false, vacation = true;
+        boolean weekday = true, vacation = false;
         if (!weekday || vacation){ // true if weekday is false or vacation is true
             System.out.println("James can sleep longer today!");
         }
@@ -21,8 +21,19 @@ public class Main {
 
         System.out.println();
         System.out.println("----Homework 3");
-        int a = 4985, b = 9875;
+        int a = 8999, b = 9879;
         boolean last_digit = a - (a / 10 * 10) == b - (b / 10 * 10);
-        System.out.println(a + ", " + b + ", " + last_digit);
+        System.out.println(a +" " + a / 10 * 10);
+        System.out.println(a + ", " + b + ", " + last_digit + "-legit");
+
+        System.out.println();
+        System.out.println("----Homework 4");
+        int n = 1;
+        while ((n % 387 != 0) || (n % 6381 != 0)) n++;
+        System.out.println("First value which is divisible by 387 and 6381 is " + n);
+
+        System.out.println();
+        System.out.println("----Homework 5");
+
     }
 }
